b647db8569f163e582565a2102319d8fe87c6cc3
Giovanna Monteiro <giovannams3999@gmail.com>
2022-04-18 19:53:14 -0300

    estilizacao dos botoes

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,19 +1,49 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82, 82, 82, 1); 
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+
   final String text; //espera receber uma string
+  final bool big; //para botões maiores
+  final Color color;
 
   Button({
     required this.text, //recebe um text por parâmetro
+    this.big = false, //false por padrão
+    this.color = DEFAULT,
+  });
+
+  Button.big({
+  required this.text, //recebe um text por parâmetro
+  this.big = true, //true por padrão
+  this.color = DEFAULT,
+  });
+
+  Button.operation({
+  required this.text, //recebe um text por parâmetro
+  this.big = false, //false por padrão
+  this.color = OPERATION,
   });
 
   @override
   Widget build(BuildContext context) {
     return Expanded( //expande a fileira de botões por toda a largura da tela
-      flex: 1, 
+      flex: big ? 2 : 1, //se for um botão grande é flex 2 senão é 1
       child: RaisedButton(
-        child: Text(text), //um child do tipo texto com o text recebido por parâmetro
+        color: this.color, //cor do fundo dos botões
+        child: Text(
+          text,
+          style: TextStyle( //estiliza os valores dos botões
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w200,
+          ),
+        ), //um child do tipo texto com o text recebido por parâmetro
         onPressed: () {},
       ),
     );
--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -12,19 +12,32 @@ class Keyboard extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ButtonRow([
+            Button.big(text: 'AC'),
+            Button(text: '%'),
+            Button.operation(text: '/'),
+          ]),
+          ButtonRow([
             Button(text: '7'),
             Button(text: '8'),
             Button(text: '9'),
+            Button.operation(text: 'X'),
           ]),
           ButtonRow([
             Button(text: '4'),
             Button(text: '5'),
             Button(text: '6'),
+            Button.operation(text: '-'),
           ]),
           ButtonRow([
             Button(text: '1'),
             Button(text: '2'),
             Button(text: '3'),
+            Button.operation(text: '+'),
+          ]),
+          ButtonRow([
+            Button.big(text: '0'),
+            Button(text: ','),
+            Button.operation(text: '='),
           ]),
         ],
       ),
